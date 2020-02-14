0350f6ebe50b4522601c9318b01a123f5967f853
Ivailo Alexandrov <ivailo.alexandrov@icloud.com>
2020-02-14 12:07:53 +0200

    Initial Commit

---
+++ Stack View Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stack View Demo
+//
+//  Created by Ivailo Alexandrov on 14.02.20.
+//  Copyright © 2020 Ivailo Alexandrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
